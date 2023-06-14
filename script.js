7ca2049cae5423da3c71183e85ed642c724dd91e
Ashley Keeling <ashleykeeling2020@gmail.com>
2023-06-14 19:23:07 +0100

    added event listeners to buttons

---
+++ script.js
@@ -0,0 +1,64 @@
+let userNum;
+let operator;
+let total;
+
+const btnDel = document.getElementById('btn-del');
+const btnClear = document.getElementById('btn-clear');
+const btnMultiply = document.getElementById('btn-multiply');
+const btnDivide = document.getElementById('btn-divide');
+const btnPlus = document.getElementById('btn-plus');
+const btnMinus = document.getElementById('btn-minus');
+const btnDot = document.getElementById('btn-dot');
+const btnEqual = document.getElementById('btn-equal');
+
+btnDel.addEventListener('click', calcDelete);
+btnClear.addEventListener('click', calcClear);
+btnMultiply.addEventListener('click', function(){
+    calcOperator('*');
+});
+btnDivide.addEventListener('click', function(){
+    calcOperator('/');
+});
+btnPlus.addEventListener('click', function(){
+    calcOperator('+');
+});
+btnMinus.addEventListener('click', function(){
+    calcOperator('-');
+});
+btnDot.addEventListener('click', calcDot);
+btnEqual.addEventListener('click', calcEqual);
+
+// adds event listners for btn 0-9
+for (let i = 0; i < 10; i++) {
+  let btn = document.getElementById('btn-' + i);
+  btn.addEventListener('click', function() {
+    calcNumber(i);
+  });
+}
+
+function calcDelete(){
+    console.log('del');
+}
+
+function calcClear(){
+    console.log('clear');
+}
+
+function calcOperator(operator) {
+    console.log(operator);
+    return operator;
+}
+
+function calcDot(){
+    console.log('.');
+}
+
+function calcEqual(){
+    console.log('=');
+}
+
+function calcNumber(num) {
+  console.log(num);
+  return num;
+}
+
